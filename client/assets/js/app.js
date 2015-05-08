813a5b47bcbd5d40cdf83d89dcb5ff3c300b8c64
rollandwalsh <rollandwalsh@gmail.com>
2015-05-08 15:18:35 -0700

    Add reviews directive

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -20,6 +20,17 @@
     .controller('SocialNav', function() {
       this.nav = socialNav;
     })
+    
+    .controller('ActingReviews', function() {
+      this.reviews = actingReviews;
+    })
+    
+    .directive('actingReviews', function() {
+      return {
+        restrict: 'E',
+        templateUrl: 'partials/acting-reviews.html'
+      }
+    })
   ;
 
   config.$inject = ['$urlRouterProvider', '$locationProvider'];
@@ -41,6 +52,9 @@
   
   var mainNav = [
     {
+      page: 'home',
+      text: 'Home'
+    },{
       page: 'about',
       text: 'About'
     },{
@@ -76,6 +90,38 @@
       site: 'GitHub',
       icon: 'github',
       link: 'https://github.com/rollandwalsh'
+    }
+  ]
+  
+  var actingReviews = [
+    {
+      production: 'Static',
+      reviewer: 'Barry Johnsaon',
+      publication: 'Oregon ArtsWatch',
+      quote: 'Rolland Walsh makes an irrepressible, even madcap Chris, the sort of fellow who injects every situation and conversation with life and surprise',
+      link: 'http://www.orartswatch.org/third-rail-the-ghost-in-the-static/',
+      title: 'Third Rail: The Ghost in Static'
+    },{
+      production: 'The Night Alive',
+      reviewer: 'Bob Hicks',
+      publication: 'Oregon ArtsWatch',
+      quote: 'Rolland Walsh shows up, swinging his fists and his attitude with equally derisive precision',
+      link: 'http://www.orartswatch.org/let-the-night-light-shine/',
+      title: 'Let the ‘Night’ light shine'
+    },{
+      production: 'The Night Alive',
+      reviewer: 'Richard Wattenberg',
+      publication: 'The Oregonian',
+      quote: 'Rolland Walsh\'s Kenneth has a certain amount of charm even as he menaces, but when he goes after Doc, he is truly horrifying',
+      link: 'http://www.oregonlive.com/performance/index.ssf/2015/02/the_night_alive_a_contemporary.html',
+      title: '\'The Night Alive\' a contemporary fairy tale, artfully crafted'
+    },{
+      production: 'Static',
+      reviewer: 'Barry Johnsaon',
+      publication: 'Oregon ArtsWatch',
+      quote: 'Rolland Walsh is hilarious<cite>Barry Johnson, Oregon ArtsWatch',
+      link: 'http://www.orartswatch.org/noises-off-is-farces-all-the-way-down/',
+      title: '\'Noises Off\’ is farces all the way down'
     }
   ]
 
